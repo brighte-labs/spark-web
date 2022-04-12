d537f2325d94dd0d4b06923180f51cfaa0d14027
Justin <justin.nhan@brighte.com.au>
2022-04-12 16:28:11 +1000

    refactor: fixed indent of text area code

--- packages/text-area/src/useTextArea.ts
+++ packages/text-area/src/useTextArea.ts
@@ -3,25 +3,25 @@ import { useInput } from '@spark-web/text-input';
 import { useTheme } from '@spark-web/theme';
 
 export function useTextAreaStyles({ disabled, invalid }: UseInputProps) {
-    const theme = useTheme();
-    const inputStyles = useInput({
-      disabled,
-      invalid,
-    });
+  const theme = useTheme();
+  const inputStyles = useInput({
+    disabled,
+    invalid,
+  });
 
-    return {
-      ...inputStyles,
+  return {
+    ...inputStyles,
 
-      // Text inputs have a fixed height, so we need to override it back to `auto`
-      height: 'auto',
-      minHeight: theme.sizing.medium,
+    // Text inputs have a fixed height, so we need to override it back to `auto`
+    height: 'auto',
+    minHeight: theme.sizing.medium,
 
-      paddingTop: theme.spacing.small,
-      paddingBottom: theme.spacing.small,
-      resize: 'vertical',
+    paddingTop: theme.spacing.small,
+    paddingBottom: theme.spacing.small,
+    resize: 'vertical',
 
-      ':invalid': {
-        color: theme.color.foreground.muted,
-      },
-    } as const;
-  }
+    ':invalid': {
+      color: theme.color.foreground.muted,
+    },
+  } as const;
+}
