282f340d266ad9758de618883eb8af967eb8ac23
Luke Bennett <hello@lukebennett.com.au>
2022-04-27 16:56:42 +1000

    Add comment

--- packages/utils/src/use-isomorphic-layout-effect.ts
+++ packages/utils/src/use-isomorphic-layout-effect.ts
@@ -5,7 +5,7 @@ import { useLayoutEffect } from 'react';
  * This is because neither `useLayoutEffect` nor `useEffect` run on the server.
  * We use this safe version which suppresses the warning by replacing it with a noop on the server.
  *
- * See: https://reactjs.org/docs/hooks-reference.html#uselayouteffect
+ * @see https://reactjs.org/docs/hooks-reference.html#uselayouteffect
  */
 export const useIsomorphicLayoutEffect =
   typeof window !== 'undefined' ? useLayoutEffect : () => {};
