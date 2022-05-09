965a687e159cea42595f092488aa191db69626b0
Chris Mitchell <chris.mitchell@brighte.com.au>
2022-05-09 16:15:00 +1000

    SPRK-56 - Update Box Component tests to include renderComponent method

--- packages/box/src/Box.test.tsx
+++ packages/box/src/Box.test.tsx
@@ -1,21 +1,25 @@
 import { cleanup, render } from '@testing-library/react';
 
+import type { BoxProps } from './Box';
 import { Box } from './Box';
+
+const renderComponent = (props: BoxProps) =>
+  render(<Box as={props?.asElement} data={props?.data} />);
 
 describe('Alert component', () => {
   afterEach(cleanup);
 
   it('should render correctly with the minimum set of props, without crashing', () => {
-    const { container } = render(<Box />);
+    const { container } = renderComponent({});
     expect(container).toBeDefined();
   });
   it('should render as a div by default', () => {
-    const { container } = render(<Box />);
+    const { container } = renderComponent({});
     const tagEl = container.querySelector('div');
     expect(tagEl).toBeTruthy();
   });
   it('should render as a link if passed in', () => {
-    const { container } = render(<Box as={'link'} />);
+    const { container } = renderComponent({ asElement: 'link' });
     const linkEl = container.querySelector('link');
     const divEl = container.querySelector('div');
     expect(linkEl).toBeTruthy();
@@ -23,7 +27,7 @@ describe('Alert component', () => {
   });
   it('should spread data attributes when they are passed in', () => {
     const data = { testAttr: 'some attr' };
-    const { container } = render(<Box as={'link'} data={data} />);
+    const { container } = renderComponent({ asElement: 'link', data });
     const divEl = container.querySelector('link');
     expect(divEl?.getAttribute('data-testAttr')).toEqual('some attr');
   });
