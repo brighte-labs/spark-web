e35394a1e2ccfedef906e87c230d9624ac25deb0
Justin <justin.nhan@brighte.com.au>
2022-05-17 14:00:32 +1000

    fix: removed prohibited import from src files in monorepo by duplicating code in password input component

--- packages/password-input/src/PasswordInput.tsx
+++ packages/password-input/src/PasswordInput.tsx
@@ -1,8 +1,11 @@
-import { getPreventableClickHandler } from '@spark-web/button/src/Button';
 import { useFieldContext } from '@spark-web/field';
 import { EyeIcon, EyeOffIcon } from '@spark-web/icon';
 import type { TextInputProps } from '@spark-web/text-input';
 import { InputAdornment, TextInput } from '@spark-web/text-input';
+import type {
+  ButtonHTMLAttributes,
+  MouseEvent as ReactMouseEvent,
+} from 'react';
 import { forwardRef, useState } from 'react';
 
 import { IconButton } from './IconButton';
@@ -40,3 +43,23 @@ export const PasswordInput = forwardRef<HTMLInputElement, PasswordInputProps>(
 );
 
 PasswordInput.displayName = 'PasswordInput';
+
+/**
+ * Prevent click events when the component is "disabled".
+ * Note: we don't want to actually disable a button element for several reasons.
+ * One being because that would prohibit the use of tooltips.
+ */
+export function getPreventableClickHandler(
+  onClick: ButtonHTMLAttributes<HTMLButtonElement>['onClick'],
+  disabled: boolean
+) {
+  return function handleClick(
+    event: ReactMouseEvent<HTMLButtonElement, MouseEvent>
+  ) {
+    if (disabled) {
+      event.preventDefault();
+    } else {
+      onClick?.(event);
+    }
+  };
+}
