e2dff08d2cc141ac69cf6a400d95dcb28664d72e
Luke Bennett <hello@lukebennett.com.au>
2022-05-18 17:40:34 +1000

    Add fonts as global styles

--- packages/core/src/fonts.tsx
+++ packages/core/src/fonts.tsx
@@ -1,12 +1,23 @@
-/**
- * TODO: the CSS file that loads Aestetico is a bit of a hack as it relies on the atomic deploys never changing
- * we should host this file on the brighte.com.au domain (or subdomain) in the future
- */
+import { Global } from '@emotion/react';
+
 export function AesteticoStylesheet(): JSX.Element {
-  return (
-    <link
-      rel="stylesheet"
-      href="https://spark-web-docs-fa4i8lp4e-brighte.vercel.app/fonts.css"
-    />
-  );
+  return <Global styles={fonts} />;
 }
+
+const fonts = `
+@import "//hello.myfonts.net/count/46e60e";
+@font-face {
+  font-family: Aestetico;
+  font-style: normal;
+  font-weight: 400;
+  font-display: swap;
+  src: url(https://spark-web-docs-fa4i8lp4e-brighte.vercel.app/fonts/aestetico-regular.woff2) format("woff2");
+}
+@font-face {
+  font-family: Aestetico;
+  font-style: normal;
+  font-weight: 600;
+  font-display: swap;
+  src: url(https://spark-web-docs-fa4i8lp4e-brighte.vercel.app/fonts/aestetico-semibold.woff2) format("woff2");
+}
+`.trim();
