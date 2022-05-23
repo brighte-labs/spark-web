7b391e45c37550b3ed50538a7723f2ad070c636d
Luke Bennett <hello@lukebennett.com.au>
2022-05-23 10:37:43 +1000

    Comment out Brighte subdomain link

--- packages/core/src/fonts.tsx
+++ packages/core/src/fonts.tsx
@@ -4,6 +4,11 @@ export function AesteticoStylesheet(): JSX.Element {
   return <Global styles={fonts} />;
 }
 
+/**
+ * Note: fonts being loaded from the Brighte subdomain are currently being
+ * blocked by CORS hence it's commented out here and we are using the Vercel
+ * deploy preview link instead.
+ */
 const fonts = `
 @import "//hello.myfonts.net/count/46e60e";
 @font-face {
@@ -11,13 +16,15 @@ const fonts = `
   font-style: normal;
   font-weight: 400;
   font-display: swap;
-  src: url(https://static-assets.prod.cloud.brighte.com.au/fonts/Aestetico-Regular.woff2) format("woff2");
+  /* src: url(https://static-assets.prod.cloud.brighte.com.au/fonts/Aestetico-Regular.woff2) format("woff2"); */
+  src: url(https://spark-web-docs-fa4i8lp4e-brighte.vercel.app/fonts/aestetico-regular.woff2) format("woff2");
 }
 @font-face {
   font-family: Aestetico;
   font-style: normal;
   font-weight: 600;
   font-display: swap;
-  src: url(https://static-assets.prod.cloud.brighte.com.au/fonts/Aestetico-SemiBold.woff2) format("woff2");
+  /* src: url(https://static-assets.prod.cloud.brighte.com.au/fonts/Aestetico-SemiBold.woff2) format("woff2"); */
+  src: url(https://spark-web-docs-fa4i8lp4e-brighte.vercel.app/fonts/aestetico-semibold.woff2) format("woff2");
 }
 `.trim();
