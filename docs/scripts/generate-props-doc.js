8b5e06aad86cfb8eb5fa0f2de9c9ccd2d9daeee5
Jess Telford <hi+github@jes.st>
2022-05-27 11:37:27 +1000

    Generate props in contentlayer

--- docs/scripts/generate-props-doc.js
+++ docs/scripts/generate-props-doc.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 const path = require('path');
+const fs = require('fs');
 
 const docgen = require('react-docgen-typescript').withCompilerOptions(
   {
@@ -33,7 +34,24 @@ const docgen = require('react-docgen-typescript').withCompilerOptions(
 
 // const resolvedPath = path.resolve('../../packages/divider/src/Divider.tsx');
 // const resolvedPath = path.resolve('../../packages/box/src/Box.tsx');
-const resolvedPath = path.resolve('../../packages/button/src/Button.tsx');
 // const resolvedPath = path.resolve('../../packages/accordion/src/Accordion.tsx');
-// Parse a file for docgen info
-console.log(JSON.stringify(docgen.parse(resolvedPath), null, 2));
+
+const extensions = ['js', 'jsx', 'json', 'ts', 'tsx', 'tson'];
+const exportRegex = /export {[^}]*} from ['"]([^;"]*)['"]/gs;
+
+export const generateProps = sourceFileDir => {
+  const packageSrcDir = path.join(sourceFileDir, 'src');
+
+  const fileData = fs
+    .readFileSync(path.join(packageSrcDir, 'index.ts'))
+    .toString();
+
+  const matches = Array.from(fileData.matchAll(exportRegex)).flatMap(
+    // Convert into an absolute path with all possible extensions
+    ([, filename]) =>
+      extensions.map(ext => `${path.resolve(packageSrcDir, filename)}.${ext}`)
+  );
+
+  // Parse all the files for docgen info
+  return docgen.parse(matches);
+};
